b5afaa9a1c186a7552d2af7f5f90849713fc23e8
clarkdenunzio <zhuclarkx205@yahoo.com>
2017-03-08 11:57:37 -1000

    added app with gitignore

---
+++ app/.meteor/local/build/programs/web.browser/packages/global-imports.js
@@ -0,0 +1,43 @@
+/* Imports for global scope */
+
+Mongo = Package.mongo.Mongo;
+Session = Package.session.Session;
+$ = Package.jquery.$;
+jQuery = Package.jquery.jQuery;
+Tracker = Package.tracker.Tracker;
+Deps = Package.tracker.Deps;
+_ = Package.underscore._;
+EJSON = Package.ejson.EJSON;
+check = Package.check.check;
+Match = Package.check.Match;
+AutoForm = Package['aldeed:autoform'].AutoForm;
+FlowRouter = Package['kadira:flow-router'].FlowRouter;
+ActiveRoute = Package['zimme:active-route'].ActiveRoute;
+BlazeLayout = Package['kadira:blaze-layout'].BlazeLayout;
+Meteor = Package.meteor.Meteor;
+global = Package.meteor.global;
+meteorEnv = Package.meteor.meteorEnv;
+WebApp = Package.webapp.WebApp;
+DDP = Package['ddp-client'].DDP;
+LaunchScreen = Package['launch-screen'].LaunchScreen;
+Blaze = Package.ui.Blaze;
+UI = Package.ui.UI;
+Handlebars = Package.ui.Handlebars;
+Spacebars = Package.spacebars.Spacebars;
+Template = Package['templating-runtime'].Template;
+meteorInstall = Package.modules.meteorInstall;
+Buffer = Package.modules.Buffer;
+process = Package.modules.process;
+Symbol = Package['ecmascript-runtime'].Symbol;
+Map = Package['ecmascript-runtime'].Map;
+Set = Package['ecmascript-runtime'].Set;
+meteorBabelHelpers = Package['babel-runtime'].meteorBabelHelpers;
+Promise = Package.promise.Promise;
+Collection2 = Package['aldeed:collection2-core'].Collection2;
+SimpleSchema = Package['aldeed:simple-schema'].SimpleSchema;
+MongoObject = Package['aldeed:simple-schema'].MongoObject;
+Accounts = Package['accounts-base'].Accounts;
+Autoupdate = Package.autoupdate.Autoupdate;
+Reload = Package.reload.Reload;
+HTML = Package.htmljs.HTML;
+
